168c3699cdbc79d67e881bc8426d3760923da516
dmitrymyshalov <dmitry.myshalov@bbraun.com>
2024-11-26 09:56:24 +0300

    Add files via upload

---
+++ Models/Calc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MVVM.Models
+{
+    internal static class Calc
+    {
+        public static double Circumference(double radius) =>
+            2 * Math.PI * radius;
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using MVVM.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MVVM.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private double _radius;
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _circumference;
+        public double Circumference
+        {
+            get => _circumference;
+            set
+            {
+                _circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Circumference = Calc.Circumference(Radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            return _radius > 0;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
